5101d2ae03463c926076c2dabd26a27fa593f029
KK01101011 <orkunkaansimsek@gmail.com>
2024-11-09 13:10:05 +0300

    Remove unused flappy.js file and its associated sprite loading function

--- client/src/scenes/flappy.js
+++
@@ -1,7 +0,0 @@
-import { k } from "../init";
-import { tweenFunc, overlay, createCoolText } from "../utils";
-
-function loadSprites() {
-	k.loadSprite("bean", "sprites/bean.png");
-}
-loadSprites();
