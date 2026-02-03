f441ccaf23f2568a57d042ff48d2c701732f0745
Orkun Kaan Şimşek <orkunkaansimsek@gmail.com>
2026-02-03 21:05:52 +0300

    Cap pixel density to reduce menu GPU load

--- client/src/init.js
+++ client/src/init.js
@@ -1,10 +1,12 @@
 import kaplay from "kaplay";
 
+const pixelDensity = Math.min(2, devicePixelRatio || 1);
+
 export const k = kaplay({
 	width: 1280,
 	height: 720,
 	texFilter: "nearest",
 	maxFPS: 60,
-	pixelDensity: devicePixelRatio,
+	pixelDensity,
 	debug: false,
 });
